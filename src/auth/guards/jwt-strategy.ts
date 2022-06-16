457052dd93fd8f560dee046871e2061a27d26ed6
Berlanda <Michele.Berlanda@siag.it>
2022-06-16 15:57:38 +0200

    files added

---
+++ src/auth/guards/jwt-strategy.ts
@@ -0,0 +1,20 @@
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'JWT_SECRET',
+    });
+  }
+
+  async validate(payload: any) {
+    return { ...payload.user };
+  }
+}
